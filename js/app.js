12124009ba6015881f21b2fd91e9d2e6545e2253
movilla <moviglez@gmail.com>
2015-01-26 14:06:49 +0100

    parriba4

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
 
 function myFunction() {
-yourArray = ['1', '2', '3', '4', '5', '6', '7', '8']
-yourArray.sort(function() { return 0.5 - Math.random() });
-var azul_1d = yourArray[0]
-var azul_2d = yourArray[1]
-var azul_3 = yourArray[2]
-var azul_4 = yourArray[3]
-var azul_5 = yourArray[4]
-var azul_6 = yourArray[5]
-var azul_7 = yourArray[6]
-var azul_8 = yourArray[7]
-document.getElementById("demo").innerHTML = azul_1d;
-document.getElementById("demo_1").innerHTML = azul_2d;
-document.getElementById("demo_2").innerHTML = azul_3;
-document.getElementById("demo_3").innerHTML = azul_4;
-document.getElementById("demo_4").innerHTML = azul_5;
-document.getElementById("demo_5").innerHTML = azul_6;
-document.getElementById("demo_6").innerHTML = azul_7;
-document.getElementById("demo_7").innerHTML = azul_8;
+        yourArray = ['1', '2', '3', '4', '5', '6', '7', '8']
+        yourArray.sort(function() { return 0.5 - Math.random() });
+        var azul_1d = yourArray[0]
+        var azul_2d = yourArray[1]
+        var azul_3 = yourArray[2]
+        var azul_4 = yourArray[3]
+        var azul_5 = yourArray[4]
+        var azul_6 = yourArray[5]
+        var azul_7 = yourArray[6]
+        var azul_8 = yourArray[7]
+
+document.getElementById("azul_demo").innerHTML = yourArray[0];
+document.getElementById("azul_1").innerHTML = yourArray[1];
+document.getElementById("azul_2").innerHTML = yourArray[2];
+document.getElementById("azul_3").innerHTML = yourArray[3];
+document.getElementById("azul_4").innerHTML = yourArray[4];
+document.getElementById("azul_5").innerHTML = yourArray[5];
+document.getElementById("azul_6").innerHTML = yourArray[6];
+document.getElementById("azul_7").innerHTML = yourArray[7];
 
         yourArray2 = ['9', '10', '11', '12', '13', '14', '15', azul_7, azul_8]
         yourArray2.sort(function() { return 0.5 - Math.random() });
@@ -30,15 +31,15 @@ document.getElementById("demo_7").innerHTML = azul_8;
         var verde_7 = yourArray2[6]
         var verde_8 = yourArray2[7]
         var verde_9 = yourArray2[8]
-        document.getElementById("demo_8").innerHTML = verde_1;
-        document.getElementById("demo_9").innerHTML = verde_2;
-        document.getElementById("demo_10").innerHTML = verde_3;
-        document.getElementById("demo_11").innerHTML = verde_4;
-        document.getElementById("demo_12").innerHTML = verde_5;
-        document.getElementById("demo_13").innerHTML = verde_6;
-        document.getElementById("demo_14").innerHTML = verde_7;
-        document.getElementById("demo_15").innerHTML = verde_8;
-        document.getElementById("demo_16").innerHTML = verde_9;
+
+document.getElementById("verde_demo").innerHTML = yourArray2[0];
+document.getElementById("verde_1").innerHTML = yourArray2[1];
+document.getElementById("verde_2").innerHTML = yourArray2[2];
+document.getElementById("verde_3").innerHTML = yourArray2[3];
+document.getElementById("verde_4").innerHTML = yourArray2[4];
+document.getElementById("verde_5").innerHTML = yourArray2[5];
+document.getElementById("verde_6").innerHTML = yourArray2[6];
+document.getElementById("verde_7").innerHTML = yourArray2[7];
 
         yourArray3 = [ verde_1, verde_2, verde_3, '23', verde_4, verde_5, verde_6, '24']
         yourArray3.sort(function() { return 0.5 - Math.random() });
@@ -50,19 +51,219 @@ document.getElementById("demo_7").innerHTML = azul_8;
         var verde_6d = yourArray3[5]
         var verde_7d = yourArray3[6]
         var verde_8d = yourArray3[7]
-        document.getElementById("demo_17").innerHTML = verde_1d;
-        document.getElementById("demo_18").innerHTML = verde_2d;
-        document.getElementById("demo_19").innerHTML = verde_3d;
-        document.getElementById("demo_20").innerHTML = verde_4d;
-        document.getElementById("demo_21").innerHTML = verde_5d;
-        document.getElementById("demo_22").innerHTML = verde_6d;
-        document.getElementById("demo_23").innerHTML = verde_7d;
-        document.getElementById("demo_24").innerHTML = verde_8d;
-        
+
+document.getElementById("verde_demod").innerHTML = yourArray3[0];
+document.getElementById("verde_1d").innerHTML = yourArray3[1];
+document.getElementById("verde_2d").innerHTML = yourArray3[2];
+document.getElementById("verde_3d").innerHTML = yourArray3[3];
+document.getElementById("verde_4d").innerHTML = yourArray3[4];
+document.getElementById("verde_5d").innerHTML = yourArray3[5];
+document.getElementById("verde_6d").innerHTML = yourArray3[6];
+document.getElementById("verde_7d").innerHTML = yourArray3[7];
+
+        yourArray4 = [ '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45']
+        yourArray4.sort(function() { return 0.5 - Math.random() });
+        var amarilla_1 = yourArray4[0]
+        var amarilla_2 = yourArray4[1]
+        var amarilla_3 = yourArray4[2]
+        var amarilla_4 = yourArray4[3]
+        var amarilla_5 = yourArray4[4]
+        var amarilla_6 = yourArray4[5]
+        var amarilla_7 = yourArray4[6]
+        var amarilla_8 = yourArray4[7]
+        var amarilla_9 = yourArray4[8]
+        var amarilla_10 = yourArray4[9]
+
+document.getElementById("amarilla_demo").innerHTML = yourArray4[0];
+document.getElementById("amarilla_1").innerHTML = yourArray4[1];
+document.getElementById("amarilla_2").innerHTML = yourArray4[2];
+document.getElementById("amarilla_3").innerHTML = yourArray4[3];
+document.getElementById("amarilla_4").innerHTML = yourArray4[4];
+document.getElementById("amarilla_5").innerHTML = yourArray4[5];
+document.getElementById("amarilla_6").innerHTML = yourArray4[6];
+document.getElementById("amarilla_7").innerHTML = yourArray4[7];
+document.getElementById("amarilla_8").innerHTML = yourArray4[8];
+document.getElementById("amarilla_9").innerHTML = yourArray4[9];
+
+        yourArray5 = [ amarilla_1, amarilla_2, amarilla_3, verde_7, amarilla_4, amarilla_5, verde_8, amarilla_6, amarilla_7, amarilla_8, amarilla_9, amarilla_10, verde_9]
+        yourArray5.sort(function() { return 0.5 - Math.random() });
+        var amarilla_1p = yourArray5[0]
+        var amarilla_2p = yourArray5[1]
+        var amarilla_3p = yourArray5[2]
+        var amarilla_4p = yourArray5[3]
+        var amarilla_5p = yourArray5[4]
+        var amarilla_6p = yourArray5[5]
+        var amarilla_7p = yourArray5[6]
+        var amarilla_8p = yourArray5[7]
+        var amarilla_9p = yourArray5[8]
+        var amarilla_10p = yourArray5[9]
+        var amarilla_11p = yourArray5[10]
+        var amarilla_12p = yourArray5[11]
+        var amarilla_13p = yourArray5[12]
+
+document.getElementById("amarilla_demop").innerHTML = yourArray5[0];
+document.getElementById("amarilla_1p").innerHTML = yourArray5[1];
+document.getElementById("amarilla_2p").innerHTML = yourArray5[2];
+document.getElementById("amarilla_3p").innerHTML = yourArray5[3];
+document.getElementById("amarilla_4p").innerHTML = yourArray5[4];
+document.getElementById("amarilla_5p").innerHTML = yourArray5[5];
+document.getElementById("amarilla_6p").innerHTML = yourArray5[6];
+document.getElementById("amarilla_7p").innerHTML = yourArray5[7];
+document.getElementById("amarilla_8p").innerHTML = yourArray5[8];
+document.getElementById("amarilla_9p").innerHTML = yourArray5[9];
+document.getElementById("amarilla_10p").innerHTML = yourArray5[10];
+document.getElementById("amarilla_11p").innerHTML = yourArray5[11];
+document.getElementById("amarilla_12p").innerHTML = yourArray5[12];
+
+        yourArray6 = [ amarilla_1p, amarilla_2p, amarilla_3p, amarilla_4p, amarilla_5p, '46', amarilla_6p, amarilla_7p, amarilla_8p, amarilla_9p, amarilla_10p, '50']
+        yourArray6.sort(function() { return 0.5 - Math.random() });
+        var amarilla_1d = yourArray6[0]
+        var amarilla_2d = yourArray6[1]
+        var amarilla_3d = yourArray6[2]
+        var amarilla_4d = yourArray6[3]
+        var amarilla_5d = yourArray6[4]
+        var amarilla_6d = yourArray6[5]
+        var amarilla_7d = yourArray6[6]
+        var amarilla_8d = yourArray6[7]
+        var amarilla_9d = yourArray6[8]
+        var amarilla_10d = yourArray6[9]
+        var amarilla_11d = yourArray6[10]
+        var amarilla_12d = yourArray6[11]
+
+document.getElementById("amarilla_demod").innerHTML = yourArray6[0];
+document.getElementById("amarilla_1d").innerHTML = yourArray6[1];
+document.getElementById("amarilla_2d").innerHTML = yourArray6[2];
+document.getElementById("amarilla_3d").innerHTML = yourArray6[3];
+document.getElementById("amarilla_4d").innerHTML = yourArray6[4];
+document.getElementById("amarilla_5d").innerHTML = yourArray6[5];
+document.getElementById("amarilla_6d").innerHTML = yourArray6[6];
+document.getElementById("amarilla_7d").innerHTML = yourArray6[7];
+document.getElementById("amarilla_8d").innerHTML = yourArray6[8];
+document.getElementById("amarilla_9d").innerHTML = yourArray6[9];
+document.getElementById("amarilla_10d").innerHTML = yourArray6[10];
+document.getElementById("amarilla_11d").innerHTML = yourArray6[11];
+
+         
+        var juego_corto = new Array();
+        juego_corto[0] = 'style/img/es/evento/DOTZ_Dorso_eventos.png';
+        juego_corto[1] = 'style/img/es/evento/'+azul_1d+'.png';
+        juego_corto[2] = 'style/img/es/evento/'+azul_2d+'.png';
+        juego_corto[3] = 'style/img/es/evento/'+verde_1d+'.png';
+        juego_corto[4] = 'style/img/es/evento/'+verde_2d+'.png';
+        juego_corto[5] = 'style/img/es/evento/'+verde_3d+'.png';
+        juego_corto[6] = 'style/img/es/evento/'+verde_4d+'.png';
+        juego_corto[7] = 'style/img/es/evento/'+verde_5d+'.png';
+        juego_corto[8] = 'style/img/es/evento/'+verde_6d+'.png';
+        juego_corto[9] = 'style/img/es/evento/'+verde_7d+'.png';
+        juego_corto[10] = 'style/img/es/evento/'+verde_8d+'.png';
+        juego_corto[11] = 'style/img/es/evento/'+amarilla_1d+'.png';
+        juego_corto[12] = 'style/img/es/evento/'+amarilla_2d+'.png';
+        juego_corto[13] = 'style/img/es/evento/'+amarilla_3d+'.png';
+        juego_corto[14] = 'style/img/es/evento/'+amarilla_4d+'.png';
+        juego_corto[15] = 'style/img/es/evento/'+amarilla_5d+'.png';
+        juego_corto[16] = 'style/img/es/evento/'+amarilla_6d+'.png';
+        juego_corto[17] = 'style/img/es/evento/'+amarilla_7d+'.png';
+        juego_corto[18] = 'style/img/es/evento/'+amarilla_8d+'.png';
+        juego_corto[19] = 'style/img/es/evento/'+amarilla_9d+'.png';
+        juego_corto[20] = 'style/img/es/evento/'+amarilla_10d+'.png';
+        juego_corto[21] = 'style/img/es/evento/'+amarilla_11d+'.png';
+        juego_corto[22] = 'style/img/es/evento/'+amarilla_12d+'.png';
+
+
+document.getElementById("demo").innerHTML = juego_corto[0];
+document.getElementById("1").innerHTML = juego_corto[1];
+document.getElementById("2").innerHTML = juego_corto[2];
+document.getElementById("3").innerHTML = juego_corto[3];
+document.getElementById("4").innerHTML = juego_corto[4];
+document.getElementById("5").innerHTML = juego_corto[5];
+document.getElementById("6").innerHTML = juego_corto[6];
+document.getElementById("7").innerHTML = juego_corto[7];
+document.getElementById("8").innerHTML = juego_corto[8];
+document.getElementById("9").innerHTML = juego_corto[9];
+document.getElementById("10").innerHTML = juego_corto[10];
+document.getElementById("11").innerHTML = juego_corto[11];
+document.getElementById("12").innerHTML = juego_corto[12];
+document.getElementById("13").innerHTML = juego_corto[13];
+document.getElementById("14").innerHTML = juego_corto[14];
+document.getElementById("15").innerHTML = juego_corto[15];
+document.getElementById("16").innerHTML = juego_corto[16];
+document.getElementById("17").innerHTML = juego_corto[17];
+document.getElementById("18").innerHTML = juego_corto[18];
+document.getElementById("19").innerHTML = juego_corto[19];
+document.getElementById("20").innerHTML = juego_corto[20];
+document.getElementById("21").innerHTML = juego_corto[21];
+document.getElementById("22").innerHTML = juego_corto[22];
+
+
+
+        var cualvemos = 0;
+
+        document.querySelector('#alante').onclick = function mover(direccion) {
+                // accedemos al objeto que contiene la imagen
+                var laimagen = document.getElementById("misfotos");
+                // accedemos al objeto que contiene el texto
+                // ¿cuál es el indice de la última imágenes en nuestra array?
+                var ultima = juego_corto.length - 1; // en el ejemplo, será el 4
+                
+                var direccion = 1;
+                // antes de cambiar los datos, en un auxilar, verificamos cuál sería la imagen a mostrar
+                var auxiliar = cualvemos + direccion; // se sumará 1 o se restará 1 al índice
+                // si el resultado es menor que cero, le decimos que vaya al otro extremo y muestre la ultima
+                if(auxiliar < 0) { auxiliar = ultima; }
+                // si el resultado es mayor que la última, le decimos que vaya al otro extremo y muestre la primera
+                if(auxiliar > ultima) { auxiliar = 0; }
+  
+                // listo, ahora ya podemos cambiar el dato sin problemas
+                cualvemos = auxiliar;
+  
+                // ponemos la dirección URL de la imagen en la etiqueta IMG
+                laimagen.src = juego_corto[cualvemos];
+
+                // ponemos el texto en la etiqueta SPAN
+                // eltexto.innerHTML = texto[cualvemos];
+                document.getElementById('atras').style.visibility = '';
+                if (cualvemos == (juego_corto.length -1)) {
+                document.getElementById('alante').style.visibility = 'hidden';
+                }
+
+        }
+
+        document.querySelector('#atras').onclick = function mover(direccion) {
+                // accedemos al objeto que contiene la imagen
+                var laimagen = document.getElementById("misfotos");
+                // accedemos al objeto que contiene el texto
+                // ¿cuál es el indice de la última imágenes en nuestra array?
+                var ultima = juego_corto.length - 1; // en el ejemplo, será el 4
+                
+                var direccion = -1;
+                // antes de cambiar los datos, en un auxilar, verificamos cuál sería la imagen a mostrar
+                var auxiliar = cualvemos + direccion; // se sumará 1 o se restará 1 al índice
+                // si el resultado es menor que cero, le decimos que vaya al otro extremo y muestre la ultima
+                if(auxiliar < 0) { auxiliar = ultima; }
+                // si el resultado es mayor que la última, le decimos que vaya al otro extremo y muestre la primera
+                if(auxiliar > ultima) { auxiliar = 0; }
+  
+                // listo, ahora ya podemos cambiar el dato sin problemas
+                cualvemos = auxiliar;
+  
+                // ponemos la dirección URL de la imagen en la etiqueta IMG
+                laimagen.src = juego_corto[cualvemos];
+
+                // ponemos el texto en la etiqueta SPAN
+                // eltexto.innerHTML = texto[cualvemos];
+                document.getElementById('alante').style.visibility = '';
+                if (cualvemos == (juego_corto.length - juego_corto.length)) {
+                document.getElementById('atras').style.visibility = 'hidden';
+                }
+        }
 
 
         var nav = document.getElementById('cartas');
-        if (nav) { nav.innerHTML = '<img id="carta" class="carta" src="style/img/es/evento/DOTZ_Dorso_eventos.png" alt="dorso"><img id="carta" class="carta" src="style/img/es/evento/'+azul_1d+'.png" alt="dorso" style="visibility: hidden;"><img id="carta" class="carta" src="style/img/es/evento/'+azul_2d+'.png" alt="dorso" style="visibility: hidden;">'; }
+        if (nav) { nav.innerHTML = '<img id="misfotos" class="carta" src="style/img/es/evento/DOTZ_Dorso_eventos.png" alt="dorso">'; }
+        if (document.getElementById('cartas').src="style/img/es/evento/DOTZ_Dorso_eventos.png") {
+                document.getElementById('atras').style.visibility = 'hidden';
+                }
+
 
         document.querySelector('#b01a').onclick = function () { Document.getElementsByClassName('carta')[1].style.visibility = ''; };
 }
